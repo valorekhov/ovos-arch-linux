e05976a5943fd8db00216953f52d1510b35c6b06
Val Orekhov <valorekhov@gmail.com>
2023-07-05 20:22:39 -0400

    Makefile generator: virtual package support

--- generate-makefile.ps1
+++ generate-makefile.ps1
@@ -3,12 +3,23 @@ Import-Module ./config-parser.psm1
 function Get-SrcInfos([System.IO.FileInfo[]]$pkgBuilds){
     $pkgBuilds | ForEach-Object {
         $srcInfoPath = "$($_.Directory)/.SRCINFO"
-        if (Test-Path $srcInfoPath){
+        $scrInfoExists = Test-Path $srcInfoPath -PathType Leaf
+        if ($scrInfoExists -and ((Get-ChildItem -Hidden $srcInfoPath).LastWriteTime -lt $_.LastWriteTime)){
+            Remove-Item -Force $srcInfoPath
+            $scrInfoExists = $false
+        }
+        if ($scrInfoExists){
             $srcInfo = Get-Content $srcInfoPath
         } else {
-            set-location $_.Directory.FullName
-            $srcInfo = makepkg --printsrcinfo
-            $srcInfo | Set-Content -Path $srcInfoPath
+            Push-Location
+            try {
+                Set-Location $_.Directory.FullName
+                Write-Host "Generating SRCINFO for $($_.Directory.FullName)"
+                $srcInfo = makepkg --printsrcinfo
+                $srcInfo | Set-Content -Path $srcInfoPath
+            } finally {
+                Pop-Location
+            }
         }
         $ret = Get-ConfigFromLines $srcInfo
         $ret['_pkggroup'] = if ($_.Directory.FullName.Contains('-extra')) {"extra"} else {"main"}
@@ -27,27 +38,21 @@ foreach($srcInfo in (Get-SrcInfos $pkgbuilds)){
     $srcInfos[$pkgname] = $srcInfo
     $deps[$pkgname] = $srcInfo.depends `
         | ForEach-Object { $_ -replace '>?=.*', '' } `
+        | ForEach-Object { 
+            if ($virtualPackages.ContainsKey($_)){
+                $virtualPackages[$_]
+            } else {
+                $_
+            }
+        }
         | Where-Object { $knownPackages -contains $_ }
 }
+$virtualPackages = Get-ConfigFromLines (Get-Content "./virtual-packages.txt") 
 
 function New-Makefile([string]$dir, $deps){
     $sorted = $deps.Keys | Sort-Object
 
-    Set-Content -Path "$dir/Makefile" `
-                -Value (".PHONY: all clean extra`nRUN_MAKEPKG := makepkg --syncdeps --noconfirm --force --install" + `
-                    "`n`nOVOS_PACKAGES := `$(notdir `$(wildcard PKGBUILDs/*))`n`nEXTRA_PACKAGES := `$(notdir `$(wildcard PKGBUILDs-extra/*))`n`nALL_PACKAGES := `$(OVOS_PACKAGES) + `$(EXTRA_PACKAGES)`n")
-
-    "# The default target will build all OVOS packages and only those 'extra' dependent packages which are in use`nall: `$(OVOS_PACKAGES)`n" | Out-File -FilePath "$dir/Makefile" -Append -Encoding "UTF8"
-
-    "extra: `$(EXTRA_PACKAGES)`n" | Out-File -FilePath "$dir/Makefile" -Append -Encoding "UTF8"
-
-    "clean: rebuild`n`t@rm -rf ./{PKGBUILDs,PKGBUILDs-extra}/*/{pkg,src}" `
-        + "`nrebuild:`n`t@echo 'Deleted any built packages, you may now run make all'`n`t@rm -rf ./{PKGBUILDs,PKGBUILDs-extra}/*/*.pkg.tar*" `
-        + "`n`nuninstall:`n`t@pacman -Qq | sort | comm -12 - <(echo `"`$(ALL_PACKAGES)`" | tr ' ' '\n' | sort) | xargs sudo pacman -Rcns --noconfirm" `
-        | Out-File -FilePath "$dir/Makefile" -Append -Encoding "UTF8"
-
-    "`n%.pkg.tar.zst:`n`t`$(eval DIR := `$(shell echo '$*' | cut -d* -f1))`n`t@echo 'Building `$(DIR)'`n`t@cd `$(DIR) && `$(RUN_MAKEPKG)`n" | Out-File -FilePath "$dir/Makefile" -Append -Encoding "UTF8"
-
+    Copy-Item -Path "$dir/templates/Makefile" -Destination "$dir/Makefile" -Force
     function Get-DepName([Parameter(ValueFromPipeline = $true)] $name){
         process {
             $srcInfo = $srcInfos[$name]
