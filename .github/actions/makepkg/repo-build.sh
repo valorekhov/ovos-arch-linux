eac36ed5af03e1f6cf31865cddee89ab642b8cb1
valorekhov <valorekhov@gmail.com>
2023-07-19 20:20:40 -0400

    Repo build updates

--- .github/actions/makepkg/repo-build.sh
+++ .github/actions/makepkg/repo-build.sh
@@ -5,4 +5,4 @@ WORKDIR="$1"
 
 MODE="repo" make -C "$WORKDIR" -f "$WORKDIR/Makefile" repo
 sudo make -C "$WORKDIR" -f "$WORKDIR/Makefile" sync-repo
-MAKEFLAGS="-j$(nproc)" MODE="repo" make -C "$WORKDIR" -f "$WORKDIR/Makefile" all
+MODE="repo" make -C "$WORKDIR" -f "$WORKDIR/Makefile" all
--- tools/pkg-build/repo-build.sh
+++ tools/pkg-build/repo-build.sh
@@ -1,4 +1,5 @@
-REPO_DIR=$1
+REPO_DIR=${1%/}
+REPO_ARCH=$(basename "$REPO_DIR")
 REPO_NAME="ovos-arch"
 LOCAL_PACMAN=${2:-"$REPO_DIR/../pacman-wrapper-$(uname -m).sh"}
 REPO_DB_FILE="$REPO_DIR/${3:-$REPO_NAME.db.tar.gz}"
@@ -7,21 +8,22 @@ PKGVER=$(grep -m1 "pkgver =" ".SRCINFO" | sed 's/.*pkgver = //g')
 PKGREL=$(grep -m1 "pkgrel =" ".SRCINFO" | sed 's/.*pkgrel = //g')
 PKG_NAMES=$(grep "pkgname =" ".SRCINFO" | sed 's/pkgname = //g')
 PKG_ARCH=$(grep "arch =" ".SRCINFO" | sed 's/arch = //g')
+PKG_BASE=$(grep "pkgbase =" ".SRCINFO" | sed 's/pkgbase = //g')
 
 echo "$PKG_NAMES"
 
 DO_BUILD=false
 if test -f "$REPO_DB_FILE" ; then
     for PKGNAME in $PKG_NAMES; do
-        echo "##### Checking if $PKGNAME-$PKGVER-$PKGREL is in the DB: $REPO_DB_FILE"
+        echo "##### Checking if $PKGNAME-$PKGVER-$PKGREL is in the $REPO_ARCH repo"
         
-        if tar -tf "$REPO_DB_FILE" | grep -m1 -F "$PKGNAME-$PKGVER-$PKGREL" ; then
+        if tar -tf "$REPO_DB_FILE" | grep -m1 -qF "$PKGNAME-$PKGVER-$PKGREL" ; then
             echo " ...... $PKGNAME-$PKGVER-$PKGREL exists in the DB"
         else
             echo " ...... $PKGNAME-$PKGVER-$PKGREL is not found. Will rebuild the entire (split) package."
             DO_BUILD=true
         fi
-        echo "DO_BUILD=$DO_BUILD"
+        # echo "DO_BUILD=$DO_BUILD"
     done
 else
     echo "##### No DB file NOT found: $REPO_DB_FILE"
@@ -35,7 +37,16 @@ if $DO_BUILD ; then
         exit 0
     fi
 
-    PACMAN="$LOCAL_PACMAN" makepkg --syncdeps --noconfirm --force || exit 13
+    # If REPO_ARCH is not x86_64, AND the package arch is 'any', AND we are not building a split package (PKG_NAMES length is 1)
+    # check if the package is already built for x86_64, in which case we can copy it here and skip the build
+
+    if [ "$REPO_ARCH" != "x86_64" ] && [ $(echo "$PKG_NAMES" | wc -w) -eq 1 ] && [ -f "$REPO_DIR/../x86_64/$PKG_NAMES-$PKGVER-$PKGREL.any.pkg.tar.{zst,xz}" ] ; then
+        echo " ...... $PKG_NAMES-$PKGVER-$PKGREL.any.pkg.tar.{zst,xz} exists in the x86_64 repo. Copying it here and skipping the build"
+        cp "$REPO_DIR/../x86_64/$PKG_NAMES-$PKGVER-$PKGREL.any.pkg.tar.{zst,xz}" "$REPO_DIR/"
+        cp "$REPO_DIR/../x86_64/$PKG_NAMES-$PKGVER-$PKGREL.any.pkg.tar.{xst,xz}.sig" "$REPO_DIR/" || true
+    else
+        MAKEFLAGS="-j$(nproc)" PACMAN="$LOCAL_PACMAN" makepkg --syncdeps --noconfirm --force || exit 13
+    fi
 
     zst_files=( *.pkg.tar.zst )
     xz_files=( *.pkg.tar.xz )
