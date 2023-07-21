b79d517d83bef68b625d197ccfc9b7f468a0e004
valorekhov <valorekhov@gmail.com>
2023-07-21 17:29:01 -0400

    ovos-dashboard: add license

--- .github/actions/makepkg/makefile-update.sh
+++ .github/actions/makepkg/makefile-update.sh
@@ -7,30 +7,26 @@ ensure_srcinfo() {
     local pkg_build="$1"
     local force_rebuild="${2:-${INPUT_REBUILDALL:-"0"}}"
     local src_info_path="${pkg_build%/*}/.SRCINFO"
-    local src_info_exists=false
 
-    if [ -f "$src_info_path" ]; then
-        src_info_exists=true
-        src_info_last_write=$(stat -c %Y "$src_info_path")
-        pkg_last_write=$(stat -c %Y "$pkg_build")
+    src_info_last_write=$(stat -c %Y "$src_info_path")
+    pkg_last_write=$(stat -c %Y "$pkg_build")
 
-        if  [ $force_rebuild = 1 ]; then
-            echo "Forcing rebuild of $pkg_build"
-        fi
-        
-        if [ "$pkg_last_write" -gt "$src_info_last_write" ] || [ "$force_rebuild" = 1 ]; then
-            pushd "${pkg_build%/*}" > /dev/null
-            echo "Generating SRCINFO for ${pkg_build%/*}"
-            makepkg --printsrcinfo > "$src_info_path" 
-            popd > /dev/null
-        fi
+    if  [ $force_rebuild = 1 ]; then
+        echo "Forcing rebuild of $pkg_build"
+    fi
+    
+    if [ "$pkg_last_write" -gt "$src_info_last_write" ] || [ "$force_rebuild" = 1 ]; then
+        pushd "${pkg_build%/*}" > /dev/null
+        echo "Generating SRCINFO for ${pkg_build%/*}"
+        makepkg --printsrcinfo > "$src_info_path" 
+        popd > /dev/null
     fi
 }
 
 echo "WORKDIR: $WORKDIR"
 echo "Running as: $(whoami) with UID: $UID"
 
-echo "Makefile dependencies RebuildAll: $INPUT_REBUILDALL; Packages: $INPUT_PACKAGES"
+echo "Makefile dependencies RebuildAll: $INPUT_REBUILDALL"
 
 # if $INPUT_REBUILDALL is set to 1 or $INPUT_PACKAGES is not set or empty, then we need to ensure that the .SRCINFO files are up to date
 # Otherwise, we iterate over the packages in $INPUT_PACKAGES and explicitly re-generate the .SRCINFO files for those packages
@@ -46,6 +42,7 @@ else
         if [ "${pkg: -9}" != "PKGBUILD" ]; then
             pkg="$pkg/PKGBUILD"
         fi
+        echo "Processing $WORKDIR/$pkg"
         ensure_srcinfo "$WORKDIR/$pkg" 1
     done
 fi 
